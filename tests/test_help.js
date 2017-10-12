84b452606a04118d0a378f0a134ad1e4c6f3fbae
Youssefares <Youssefares@ymail.com>
2017-10-13 00:41:48 +0200

    fix test suite to fail instead of give unhandled promise rejection error

--- tests/test_help.js
+++ tests/test_help.js
@@ -19,7 +19,7 @@ describe('python-error-help: help()', function(){
 
 
   it('should not return null & should capture while', function(){
-    var response = help("Traceback (most recent call last):\n  File \"python\",\
+    return help("Traceback (most recent call last):\n  File \"python\",\
      line 2\n    while variable == 6\n                      ^\nSyntaxError: invalid syntax").then((response)=>{
         expect(response.id).to.equal(1)
         expect(response.groups).to.deep.equal(["while"])
@@ -27,7 +27,7 @@ describe('python-error-help: help()', function(){
   })
 
   it('should not return null & should capture if',function(){
-    var response = help("Traceback (most recent call last):\n  File \"python\", \
+    return help("Traceback (most recent call last):\n  File \"python\", \
       line 4\n    if x == y+1\n              ^\nSyntaxError: invalid syntax").then((response)=>{
         expect(response.id).to.equal(1)
         expect(response.groups).to.deep.equal(["if"])
@@ -36,7 +36,7 @@ describe('python-error-help: help()', function(){
   })
 
   it('should not return null & should capture do_sth()', function(){
-    var response = help("Traceback (most recent call last):\n  File \"python\", \
+    return help("Traceback (most recent call last):\n  File \"python\", \
       line 6\n    do_sth()\n        ^\nIndentationError: expected an indented block").then((response)=>{
         expect(response.id).to.equal(2)
         expect(response.groups).to.deep.equal(["do_sth()"])
@@ -44,15 +44,11 @@ describe('python-error-help: help()', function(){
   })
 
   it('should capture error message and error type + get proper response from file', function(){
-    var response = help("Traceback (most recent call last):\n  File \"python\", \
+    return help("Traceback (most recent call last):\n  File \"python\", \
       line 3\n   Someerror that will never exist and some numbers 35432:\n         ^\nSyntaxError: invalid syntax").then((response)=>{
         expect(response.id).to.equal(-1)
         expect(response.groups).to.deep.equal(["SyntaxError: invalid syntax", "SyntaxError"])
-        expect(response.response).to.deep.equal(
-          "This type of error occurs when the python interpreter can't\
-         understand your code. The file your trying to run or a file imported in it, contains code that\
-          doesn't follow language rules."
-        )
+        expect(response.message).to.deep.equal("This type of error occurs when the python interpreter can't understand your code. The file your trying to run or a file imported in it, contains code that doesn't follow language rules.")
     })
   })
 })
